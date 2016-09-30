fc11fca2690aa70d6f1b522ef1cd94a6f5b3eb73
Mohammed Fareed <mohamedfared95@gmail.com>
2016-09-30 16:44:59 +0200

    Initial commit

---
+++ app/src/main/java/com/todo/prework/mohammedfareed/codepathtodoapp/EditTodo.java
@@ -0,0 +1,31 @@
+package com.todo.prework.mohammedfareed.codepathtodoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditTodo extends AppCompatActivity {
+
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_todo);
+        String text = getIntent().getStringExtra("name");
+        editText = (EditText) findViewById(R.id.etEditItem);
+        editText.setText(text);
+        editText.requestFocus();
+    }
+
+    public void onSaveItem(View view) {
+        Intent result = new Intent();
+        result.putExtra("newValue", editText.getText().toString());
+        result.putExtra("position", getIntent().getIntExtra("number", 0));
+        setResult(RESULT_OK, result);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/todo/prework/mohammedfareed/codepathtodoapp/MainActivity.java
@@ -0,0 +1,99 @@
+package com.todo.prework.mohammedfareed.codepathtodoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        readItems();
+        listView = (ListView) findViewById(R.id.lvItems);
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        listViewSetUpLisetners();
+    }
+
+    private void listViewSetUpLisetners() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                Toast.makeText(MainActivity.this, "Item removed!", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, EditTodo.class);
+                intent.putExtra("name", items.get(i));
+                intent.putExtra("number", i);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1){
+            items.set(data.getIntExtra("position", 0), data.getStringExtra("newValue"));
+            itemsAdapter.notifyDataSetChanged();
+            writeItems();
+        }
+    }
+
+    public void onAddItem(View view) {
+        EditText editText = (EditText) findViewById(R.id.etNewItem);
+        if (!TextUtils.isEmpty(editText.getText().toString())) {
+            itemsAdapter.add(editText.getText().toString());
+            editText.setText("");
+            writeItems();
+        }
+        else
+            editText.setError("Can't be empty");
+    }
+
+    private void readItems(){
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir, "todo.txt");
+        try{
+            items = new ArrayList<>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems(){
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir, "todo.txt");
+        try{
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
